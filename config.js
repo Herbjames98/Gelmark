86af9fb88dd880832fef52a6d261c9f75d17de4c
Herbjames98 <hrbrtmennis@gmail.com>
2025-07-06 23:01:52 -0400

    1

--- config.js
+++ config.js
@@ -1,7 +1,6 @@
-// config.js
 export const CONFIG = {
   XP_PER_TASK: 5,
-  XP_TO_LEVEL: 100,
-  SILL_GAIN_PER_XP: 0.5,
-  GEL_DRAIN_PER_ACTION: 1
+  SILL_GAIN_PER_XP: 1,
+  GEL_DRAIN_PER_ACTION: 2,
+  XP_TO_LEVEL: 100
 };
