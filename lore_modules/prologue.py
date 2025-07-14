3a7aa1f123f12621eae727f0403eb4037a530b5f
Herbjames98 <hrbrtmennis@gmail.com>
2025-07-14 00:07:10 -0400

    Update prologue.py

--- lore_modules/prologue.py
+++ lore_modules/prologue.py
@@ -1,11 +1,12 @@
 # lore_modules/prologue.py
 
 prologue_lore = {
-"summary": "Not the warp" 
-(
-            "The protagonist, unnamed at first, arrives through a time fracture caused by the Pulse Engine, crashing "
+    "summary": "Not the warp",
+    "key events": (
+          "The protagonist, unnamed at first, arrives through a time fracture caused by the Pulse Engine, crashing "
             "in the Age of Blades near a Norse-like village. Mistaken as a fated omen, they uncover fragments of Grace, "
             "a broken AI from a collapsed future spiral."
-        ),
+    ),
+
         "arrival_event": "Meteoric descent into the Viking hinterlands; Grace’s core discovered."
     }
