c900fcbb25d1a64cd7ec7a79b991d72b42caad37
Herbjames98 <hrbrtmennis@gmail.com>
2025-07-19 02:31:46 -0400

    Create act3.py

---
+++ my_gm/lore_modules/act3.py
@@ -0,0 +1,21 @@
+# This is a blank template for a new lore act.
+# IMPORTANT: Rename the variable 'act_name_lore' to match the file.
+# For example, in 'act3.py', this should be 'act3_lore'.
+
+act3_lore = {
+    "summary": "",
+    "major_events": [],
+    "companions_bond_status": [],
+    "traits_unlocked": [],
+    "shrines_visited": [],
+    "visions_echo_sequences": [],
+    "lore_codex_expansions": [],
+    "timeline_edits": [],
+    "key_terms_introduced": [],
+    "locations_realms_visited": [],
+    "faction_threat_encounters": [],
+    "oaths_rituals_performed": [],
+    "artifacts_discovered": [],
+    "narrative_threads_opened": [],
+    "narrative_threads_closed": []
+}
